253b6008cc4c42b8477a1b2f5cdb98de1143d51b
David <dmonzonreyes@gmail.com>
2021-12-13 16:24:03 -0600

    added stopwatch

---
+++ components/stopwatch/LapTime.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './LapTime.css';
+
+import timeFormat from '../stopwatch/utils/timeFormat';
+
+class LapTime extends Component {
+
+  static proptTypes = {
+    lap: PropTypes.number,
+    time: PropTypes.number,
+    duration: PropTypes.number
+  };
+
+  static defaultProps = {
+    lap: 0,
+    time: 0,
+    duration: 0
+  };
+
+  render() {
+
+    const { lap, time,duration } = this.props;
+
+    return (
+      <div className="LapTime">
+        <span className="LapTime__lap" >{lap}</span>
+        <span className="LapTime__time" >{timeFormat(time)}</span>
+        <span className='LapTime__time'>{timeFormat(duration)}</span>
+      </div>
+    );
+  }
+}
+
+export default LapTime;
---
+++ screens/TabOneScreen.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { Stopwatch } from '../components/stopwatch';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Stopwatch/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
